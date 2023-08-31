dd73c0c0438f46ecd76864b5839f4c9b27b36a55
BorisKly <bklykavka@gmail.com>
2023-08-31 13:00:08 +0300

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Borys Klykavka on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
